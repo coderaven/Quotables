c4237715b28a90f9be033f3e477f15586280b0e8
Raven G. Duran <me@imraven.com>
2012-06-24 00:43:31 +0800

    Added Basic HTMl Structure

--- index.php
+++ index.php
@@ -5,6 +5,27 @@
 	<title>Quotable</title>
 </head>
 <body>
-	Welcome to Quotable!
+	<div id="wrap">
+		<header>
+			<div id="logo"><h1>Quotable</h1></div>
+			<nav id="main-nav">
+				<ul>
+					<li><a href="#">Home</a></li>
+					<li><a href="#">Random</a></li>
+					<li><a href="#">Submit</a></li>
+				</ul>
+			</nav>
+		</header>
+		<section id="main">
+			<section id="content">
+				<p>
+					This is a Test Content.
+				</p>
+				<cite>By Raven</cite>
+			</section>
+			<aside id="sidebar"></aside>
+		</section>
+		<footer></footer>
+	</div>
 </body>
 </html>
