2ad4d71a3d77a23849062feb90fb159ecddd0d5e
Raven G. Duran <me@imraven.com>
2012-06-24 21:37:12 +0800

    Added Base Style and Layout
    Added Style and Basic Layout

--- index.php
+++ index.php
@@ -3,29 +3,97 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Quotable</title>
+	<link rel="stylesheet" href="css/style.css">
+	<link href='http://fonts.googleapis.com/css?family=Marck+Script' rel='stylesheet' type='text/css'>
 </head>
 <body>
-	<div id="wrap">
-		<header>
-			<div id="logo"><h1>Quotable</h1></div>
-			<nav id="main-nav">
+	<header>
+			<div id="logo"><h1><a href="#">Quotables</a></h1></div>
+			<nav>
 				<ul>
 					<li><a href="#">Home</a></li>
 					<li><a href="#">Random</a></li>
 					<li><a href="#">Submit</a></li>
 				</ul>
 			</nav>
-		</header>
+	</header>
+	<div class="clearfix"></div>
+	<div id="wrap">
 		<section id="main">
-			<section id="content">
-				<p>
-					This is a Test Content.
-				</p>
-				<cite>By Raven</cite>
+			<section id="content" class="col">
+				<div class="quote">
+					<blockquote>
+						"Life has no smooth road for any of us; and in the bracing atmosphere 
+						of a high aim the very roughness stimulates the climber to steadier steps,
+						till the legend, over steep ways to the stars, fulfills itself. "
+					</blockquote>
+					<cite>~ Albert Einstein</cite>
+				</div>
+				<div class="quote">
+					<blockquote>
+						"Happy are those who dream dreams and are ready to pay the price to 
+						make them come true."
+					</blockquote>
+					<cite>~ Leon J. Suenes</cite>
+				</div>
+				<div class="quote">
+					<blockquote>
+						"If you do not hope, you will not find what is beyond your hopes."
+					</blockquote>
+					<cite>~ St. Clement of Alexandra</cite>
+				</div>
+				<div class="quote">
+					<blockquote>
+						"We are all inventors, each sailing out on a voyage of discovery, 
+						guided each by a private chart, of which there is no duplicate. The world 
+						is all gates, all opportunities."
+					</blockquote>
+					<cite>~ Ralph Waldo Emerson</cite>
+				</div>
+				<div class="quote">
+					<blockquote>
+						"Without inspiration the best powers of the mind remain dormant. 
+						There is a fuel in us which needs to be ignited with sparks."
+					</blockquote>
+					<cite>~ Johann Gottfried Von Herder</cite>
+				</div>
+				<div class="quote">
+					<blockquote>
+						"We are what we repeatedly do. Excellence, therefore, is not an act but a habit."
+					</blockquote>
+					<cite>~ Aristotle</cite>
+				</div>
+				<div class="quote">
+					<blockquote>
+						"Without inspiration the best powers of the mind remain dormant. 
+						There is a fuel in us which needs to be ignited with sparks."
+					</blockquote>
+					<cite>~ Johann Gottfried Von Herder</cite>
+				</div>
+				<div class="quote">
+					<blockquote>
+						"We are what we repeatedly do. Excellence, therefore, is not an act but a habit."
+					</blockquote>
+					<cite>~ Aristotle</cite>
+				</div>
+				<div class="quote">
+					<blockquote>
+						"Without inspiration the best powers of the mind remain dormant. 
+						There is a fuel in us which needs to be ignited with sparks."
+					</blockquote>
+					<cite>~ Johann Gottfried Von Herder</cite>
+				</div>
+				<div class="quote">
+					<blockquote>
+						"We are what we repeatedly do. Excellence, therefore, is not an act but a habit."
+					</blockquote>
+					<cite>~ Aristotle</cite>
+				</div>
 			</section>
-			<aside id="sidebar"></aside>
+			<aside id="sidebar" class="col">
+				
+			</aside>
 		</section>
-		<footer></footer>
 	</div>
 </body>
 </html>
